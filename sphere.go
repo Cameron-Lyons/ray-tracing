7335277490ed40b93d2fc8e4653c85ed6b0a2d84
Cameron Lyons <cameron.lyons2@gmail.com>
2022-08-13 13:25:29 -0500

    get hit record

--- sphere.go
+++ sphere.go
@@ -7,21 +7,20 @@ type sphere struct {
 	radius float32
 }
 
-func (s sphere) hit(r ray, t_min float32, t_max float32) bool {
+func (s sphere) hit(r ray, t_min float32, t_max float32, rec hit_record) bool {
 	oc := vec_sub(r.origin, s.center)
 	a := vec_dot(r.direction, r.direction)
 	b := vec_dot(oc, r.direction)
 	c := vec_dot(oc, oc) - s.radius*s.radius
 	discriminant := b*b - a*c
 	if discriminant > 0 {
-		temp := (-b - float32(math.Sqrt(float64(discriminant)))) / a
-		if temp < t_max && temp > t_min {
-			return true
+		root := (-b - float32(math.Sqrt(float64(discriminant)))) / a
+		if root < t_min || t_max < root {
+			return false
 		}
-		temp = (-b + float32(math.Sqrt(float64(discriminant)))) / a
-		if temp < t_max && temp > t_min {
-			return true
-		}
+		rec.t = root
+		rec.p = point_at_parameter(r, root)
+		rec.normal = unit_vector(vec_sub(rec.p, s.center))
 	}
 	return false
 }
