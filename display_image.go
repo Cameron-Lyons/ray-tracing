c4673c77c041769e616567058b89de6764c57374
Cameron Lyons <cameron.lyons2@gmail.com>
2022-08-12 21:05:22 -0500

    draw image

--- display_image.go
+++ display_image.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"os"
 )
 
 func main() {
@@ -9,6 +14,27 @@ func main() {
 	const image_width int = 256
 	const image_height int = 256
 
-	fmt.Print("P3\n", image_width, ' ', image_height, "\n255\n")
+	image := image.NewRGBA(image.Rect(0, 0, image_width, image_height))
 
+	for j := image_height - 1; j >= 0; j-- {
+		for i := 0; i < image_width; i++ {
+
+			r := float64(i) / float64(image_width-1)
+			g := float64(j) / float64(image_height-1)
+			b := 0.25
+
+			ir := int(255.999 * r)
+			ig := int(255.999 * g)
+			ib := int(255.990 * b)
+
+			image.Set(i, j, color.RGBA{uint8(ir), uint8(ig), uint8(ib), 255})
+		}
+	}
+	draw.Draw(image, image.Bounds(), image, image.Bounds().Min, draw.Src)
+	out, err := os.Create("./output.png")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	png.Encode(out, image)
 }
