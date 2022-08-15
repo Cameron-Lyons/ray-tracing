f83d332f9481b6e5d42aa331d4da2b0381b82755
Cameron Lyons <cameron.lyons2@gmail.com>
2022-08-14 19:43:11 -0500

    get ray func

--- camera.go
+++ camera.go
@@ -10,4 +10,9 @@ type camera struct {
 	horizontal := Vec3{viewport_width, 0, 0}
 	vertical := Vec3{0, viewport_hieght, 0}
 	lower_left_corner := Vec3{-viewport_width / 2, -viewport_hieght / 2, -focal_length}
+	ray ray
+}
+
+func get_ray(u, v) ray {
+	return ray{origin, vec_add(vec_add(vec_add(lower_left_corner, vec_mul_scalar(horizontal, u)), vec_mul_scalar(vertical, v)), Vec3{0, 0, focal_length})}
 }
