2554123f6ad8232079c0b0dbfa2ee08d58486951
Cameron Lyons <cameron.lyons2@gmail.com>
2022-08-15 23:49:13 -0500

    add lambertian

--- material.go
+++ material.go
@@ -3,3 +3,8 @@ package main
 type material struct {
 	scatter func(ray, hit_record, attenuation Color) (Vec3, ray)
 }
+
+type lambertian struct {
+	albedo  Color
+	scatter func(ray, hit_record, attenuation Color) bool
+}
