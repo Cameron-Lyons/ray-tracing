ba3c3ac335ee40a14023fbaaeb499358c0be2c3b
Cameron Lyons <cameron.lyons2@gmail.com>
2022-08-16 20:49:56 -0500

    add metal material

--- material.go
+++ material.go
@@ -16,3 +16,13 @@ type lambertian struct {
 	}
 }
 
+type metal struct {
+	albedo  Color
+	fuzz    float32
+	scatter func(ray, hit_record, attenuation Color) bool {
+		reflected = reflect(ray.direction, rec.normal)
+		scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere())
+		attenuation = albedo
+		return scattered.direction.dot(rec.normal) > 0.0
+	}
+}
