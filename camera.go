453e311885776d7cde3c8cf32479d4ffe15e4612
Cameron Lyons <cameron.lyons2@gmail.com>
2022-08-19 19:33:53 -0500

    move camera

--- camera.go
+++ camera.go
@@ -1,16 +1,25 @@
 package main
 
 type camera struct {
-	aspectRatio := 16.0 / 9.0
-	viewport_hieght := 2.0
-	viewport_width := aspectRatio * viewport_hieght
-	focal_length := 1.0
+	vfov float32
+	aspect_ratio float32
+	theta = vfov * (math.Pi / 180)
+	half_height = math.Tan(theta / 2)
+	viewport_hieght = 2 * half_height
+	viewport_width = aspect_ratio * viewport_height
+	focal_length = 1.0
+	
+	origin = Vec3{0, 0, 0}
+	horizontal = Vec3{viewport_width, 0, 0}
+	vertical = Vec3{0, viewport_hieght, 0}
+	lower_left_corner = Vec3{-viewport_width / 2, -viewport_hieght / 2, -focal_length}
 
-	origin := Vec3{0, 0, 0}
-	horizontal := Vec3{viewport_width, 0, 0}
-	vertical := Vec3{0, viewport_hieght, 0}
-	lower_left_corner := Vec3{-viewport_width / 2, -viewport_hieght / 2, -focal_length}
-	ray ray
+	ray get_ray(u, v) ray {
+		origin,
+		direction := vec_add(vec_add(vec_add(lower_left_corner, vec_mul_scalar(horizontal, u)), vec_mul_scalar(vertical, v)), Vec3{0, 0, focal_length})
+		direction = vec_normalize(direction)
+		return ray{origin, direction}
+	}
 }
 
 func get_ray(u, v) ray {
