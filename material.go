e4e1f6a87c780f65c6ad756f02387f87dc8b148f
Cameron Lyons <cameron.lyons2@gmail.com>
2022-08-19 19:28:23 -0500

    add delectric

--- material.go
+++ material.go
@@ -14,11 +14,12 @@ type lambertian struct {
 		attenuation = albedo
 		return true
 	}
+	dielectric float64
 }
 
 type metal struct {
 	albedo  Color
-	fuzz    float32
+	fuzz    float64
 	scatter func(ray, hit_record, attenuation Color) bool {
 		reflected = reflect(ray.direction, rec.normal)
 		scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere())
@@ -26,3 +27,49 @@ type metal struct {
 		return scattered.direction.dot(rec.normal) > 0.0
 	}
 }
+
+type dielectric struct {
+	ref_idx float64
+	scatter func(ray, hit_record, attenuation Color) bool {
+		attenuation = Color{1.0, 1.0, 1.0}
+		reflected = reflect(ray.direction, rec.normal)
+		var outward_normal Vec3
+		var ni_over_nt float64
+		var cosine float64
+		if ray.direction.dot(rec.normal) > 0.0 {
+			outward_normal = -rec.normal
+			ni_over_nt = ref_idx
+			cosine = ref_idx * ray.direction.dot(rec.normal) / ray.direction.len()
+		} else {
+			outward_normal = rec.normal
+			ni_over_nt = 1.0 / ref_idx
+			cosine = -ray.direction.dot(rec.normal) / ray.direction.len()
+		}
+		var refracted Vec3
+		var reflect_prob float64
+		var refracted_prob float64
+		if refract(ray.direction, outward_normal, ni_over_nt, &refracted) {
+			reflect_prob = schlick(cosine, ref_idx)
+			refracted_prob = 1.0 - reflect_prob
+		} else {
+			scattered = ray(rec.p, reflected)
+			return true
+		}
+		if rand.Float64() < reflect_prob {
+			scattered = ray(rec.p, reflected)
+		} else {
+			scattered = ray(rec.p, refracted)
+		}
+		return true
+	}
+}
+
+func (m *material) scatter(ray, hit_record, attenuation Color) (Vec3, ray) {
+	return m.scatter(ray, hit_record, attenuation)
+}
+
+func (m *lambertian) scatter(ray, hit_record, attenuation Color) (Vec3, ray) {
+	return m.scatter(ray, hit_record, attenuation)
+}
+
+func (m *metal) scatter
